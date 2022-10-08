e2c3847b7e3bde8b63f5abff2ba30ac2c9cbc203
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-10-08 14:46:06 +0200

    VERSION: 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, David Montero Loaiza'
 author = 'David Montero Loaiza'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
